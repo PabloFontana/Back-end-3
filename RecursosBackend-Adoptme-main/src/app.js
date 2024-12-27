461f03a822419835e17d275f5beb2b870f6f400e
Pablo <pablofontanan@gmail.com>
2024-12-27 23:38:10 +0100

    agregando swagger

--- RecursosBackend-Adoptme-main/src/app.js
+++ RecursosBackend-Adoptme-main/src/app.js
@@ -9,7 +9,7 @@ import sessionsRouter from './routes/sessions.router.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const connection = mongoose.connect(`mongodb+srv://pablofontanaCH:coderhouse@cluster0.qyda2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,5 +18,25 @@ app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
+app.use("/api/mocks", mocksRouter);
 
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+
+
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUiExpress from "swagger-ui-express";
+
+
+const swaggerOptiones = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            tittle: "Documentacion de la app adoptame.",
+            description:"App dedicada a que no compres mas perros y los adoptes"
+        }
+    }, 
+    apis: ["./src/docs/**/*.yaml"]
+}
+
+const specs = swaggerJSDoc(swaggerOptiones);
+app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(spec));
